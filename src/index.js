5e32818ae1f6340fb8ada4e8c004ff68ff5fc10d
michaladamiak <mchadamiak@gmail.com>
2022-04-04 19:02:44 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const game = document.querySelector('.game')
     const score = document.querySelector('.points')
-    const win = document.getElementById('win')
-    const lose = document.getElementById('lose')
+    const end = document.getElementById('end')
+    const result = document.getElementById('result')
     const comment = document.querySelector('.score')
 
     const layout  = [
@@ -28,279 +28,188 @@ document.addEventListener('DOMContentLoaded', () => {
             1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,0,1,
             1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,1,
             1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-
-    let board = []
+    const board = []
     const size = 21
     let points = 206
-    let position = 220
+    let playerPosition = 220
+    createBoard()
 
-    for(let i=0; i<layout.length; i++){
-        board[i] = document.createElement("div")
-        switch(layout[i]){
-            case 0:
-                board[i].classList.add('ball')
-                break
-            case 1:
-                board[i].classList.add('boarder')
-                break
-            case 2:
-                board[i].classList.add('player')
-                break
-            case 3:
-                board[i].classList.add('path')
-                break
-            case 4:
-                board[i].classList.add('enemy')
-                // board[i].classList.add('ball')
-                break
-            case 5:
-                board[i].classList.add('enemy2')
-                // board[i].classList.add('ball')
-                break
-            case 6:
-                board[i].classList.add('enemy3')
-                // board[i].classList.add('ball')
-                break
-            case 7:
-                board[i].classList.add('enemy4')
-                // board[i].classList.add('ball')
-                break
+
+    function createBoard() {
+        for(let i=0; i<layout.length; i++){
+            board[i] = document.createElement("div")
+            switch(layout[i]){
+                case 0:
+                    board[i].classList.add('ball')
+                    break
+                case 1:
+                    board[i].classList.add('boarder')
+                    break
+                case 2:
+                    board[i].classList.add('player')
+                    break
+                case 3:
+                    board[i].classList.add('path')
+                    break
+                case 4:
+                    board[i].classList.add('enemy')
+                    break
+                case 5:
+                    board[i].classList.add('enemy2')
+                    break
+                case 6:
+                    board[i].classList.add('enemy3')
+                    break
+                case 7:
+                    board[i].classList.add('enemy4')
+                    break
+            }
+            game.appendChild(board[i])
         }
-        game.appendChild(board[i])
     }
 
-    //zjadanie kulek
+    function endGame() {
+        end.style.display = 'flex'
+        document.removeEventListener('keydown', move)
+        end.addEventListener('click', function() {
+            window.location.reload(true)
+        })
+        comment.innerHTML = ''
+        board[playerPosition].classList.remove('player', 'open-right', 'open-left', 'open-down', 'open-up')
+        board[playerPosition].classList.add('path')
+        clearInterval(changeMouthInterval)
+    }
+
+    //gathering points
     function ball() {
-        if(board[position].classList.contains('ball')) {
-            board[position].classList.remove('ball')
+        if(board[playerPosition].classList.contains('ball')) {
+            board[playerPosition].classList.remove('ball')
             points -= 1
             score.innerHTML = points
         }
         if(points == 0) {
-            document.removeEventListener('keydown', move)
-            win.style.display = 'flex'
-            win.addEventListener('click', function() {
-                window.location.reload(true)
-            })
-            // clearInterval(enemy_move)
-            score.innerHTML = 'gitara!'
-            comment.innerHTML = ''
-            // tutaj trzeba cos zmienic zeby nie bylo playera po wygraniu bo enenmy nadal moga zjesc
-            board[position].classList.remove('player')
-            board[position].classList.add('path')
-            // position = 0 
+            endGame()
         }
     }
 
-    const directions_before = [1,size,-1,-size]
+    //enenmy class
+    const directions = [1,size,-1,-size]
     class Enemy {
-        constructor(nazwa, pozycja, kierunek) {
-            this.nazwa = nazwa
-            this.pozycja = pozycja
-            this.warunek = true
-            this.kierunek = kierunek
-            this.pop_kier = []
+        constructor(name, enemyPosition, enemyDirection) {
+            this.name = name
+            this.enemyPosition = enemyPosition
+            this.containsBall = true
+            this.enemyDirection = enemyDirection
+            this.previousEnemyDirection = []
         }
+        //enemy movement
+        go() {
+            //clear path
+            board[this.enemyPosition].classList.remove(this.name)
+            this.containsBall ? board[this.enemyPosition].classList.add('ball') : board[this.enemyPosition].classList.add('path')
 
-        idz() {
-            //czyszczenie pola
-            board[this.pozycja].classList.remove(this.nazwa)
-            this.warunek ? board[this.pozycja].classList.add('ball') : board[this.pozycja].classList.add('path')
-
-            //ustalanie kierunku
-            let possible_directions = []
-            for(let i=0; i<directions_before.length;i++) {
-                if(!board[this.pozycja+directions_before[i]].classList.contains('boarder')) {
-                    possible_directions.push(directions_before[i])
+            //set direction
+            let possibleDirections = []
+            for(let i=0; i<directions.length;i++) {
+                if(!board[this.enemyPosition+directions[i]].classList.contains('boarder')) {
+                    possibleDirections.push(directions[i])
                 }
             }
-            let new_directions = []
-            for(let i=0; i<possible_directions.length; i++) {
-                if((!this.pop_kier.includes(possible_directions[i])) && (possible_directions[i] != -this.kierunek)) {
-                    new_directions.push(possible_directions[i])
+            let newDirections = []
+            for(let i=0; i<possibleDirections.length; i++) {
+                if((!this.previousEnemyDirection.includes(possibleDirections[i])) && (possibleDirections[i] != -this.enemyDirection)) {
+                    newDirections.push(possibleDirections[i])
                 }
             }
-            this.pop_kier = new_directions
-            if(new_directions.length>0) {
-                this.kierunek = new_directions[Math.floor(Math.random()*new_directions.length)]
+            this.previousEnemyDirection = newDirections
+            if(newDirections.length>0) {
+                this.enemyDirection = newDirections[Math.floor(Math.random()*newDirections.length)]
             }
-            //krok, sprawdzenie czy byly ball na nastepnym polu, i czy byl player
-            this.pozycja += this.kierunek
-            board[this.pozycja].classList.contains('ball') ? this.warunek = true : this.warunek = false
-            board[this.pozycja].classList.remove('ball')
-            board[this.pozycja].classList.add(this.nazwa)
-            if(this.pozycja==position) {
-                // clearInterval(inter)
-                // inter = ? 
-                // trzeba dodać coś zeby przeciwnicy sie zatrzymywali
-                document.removeEventListener('keydown', move)
-                lose.style.display = 'flex'
-                board[position].classList.remove('player')
-                lose.addEventListener('click', function() {
-                    window.location.reload(true)
-                })
-                score.innerHTML = 'lipa :/'
-                comment.innerHTML = ''
-                position = 0
+            //movement, check if point should stan on this field 
+            this.enemyPosition += this.enemyDirection
+            board[this.enemyPosition].classList.contains('ball') ? this.containsBall = true : this.containsBall = false
+            board[this.enemyPosition].classList.remove('ball')
+            board[this.enemyPosition].classList.add(this.name)
+            if(this.enemyPosition==playerPosition) {
+                result.innerHTML = 'TOO BAD<br>RESTART'
+                endGame()
             }
         }
-
+        //interval for method go
         timer() {
             var that = this
-            var inter = setInterval(function(){that.idz();}, 200);
+            var inter = setInterval(function(){that.go();}, 200);
         }
     }
 
-    let czerwony = new Enemy('enemy', 22, 1)
-    czerwony.timer()
-    let zielony = new Enemy('enemy2', 418, -1)
-    zielony.timer()
-    let rozowy = new Enemy('enemy3', 80, -21)
-    rozowy.timer()
-    let pomaranczowy = new Enemy('enemy4', 360, 21)
-    pomaranczowy.timer()
+    const red = new Enemy('enemy', 22, 1)
+    red.timer()
+    const green = new Enemy('enemy2', 418, -1)
+    green.timer()
+    const pink = new Enemy('enemy3', 80, -21)
+    pink.timer()
+    const orange = new Enemy('enemy4', 360, 21)
+    orange.timer()
 
-    const enemies = [czerwony, zielony, rozowy, pomaranczowy]
+    const enemies = [red, green, pink, orange]
 
-    
+    //opening mouth
+    function changeMouth() {
+        board[playerPosition].classList.add(mouth)
+        setTimeout(function(){
+            board[playerPosition].classList.remove('open-right', 'open-left', 'open-down','open-up')
+        },300)
+    }
 
+    //interval for opening mouth
+    let playerDirection = 1
+    let mouth = 'open-right'
+    const changeMouthInterval = setInterval(changeMouth,600)
 
-    // pierwszy model chodzenia enemy
+    //move player based on direction
+    function moveDirection(dir) {
+        if(!board[playerPosition+dir].classList.contains('boarder')) {
+            board[playerPosition].classList.remove('player')
+            board[playerPosition].classList.add('path')
+            playerPosition+=dir
+            ball()
+            board[playerPosition].classList.add('player') 
+        }
+    }
 
-    // let direction = 1
-    // let condition = true
-    // let enemy_position = 22
-    // let prev_directions = []
-
-    // function run() {
-    //     //czyszczenie pola
-    //     board[enemy_position].classList.remove('enemy')
-    //     condition ? board[enemy_position].classList.add('ball') : board[enemy_position].classList.add('path')
-
-    //     //ustalanie kierunku
-    //     let possible_directions = []
-    //     for(let i=0; i<directions_before.length;i++) {
-    //         if(!board[enemy_position+directions_before[i]].classList.contains('boarder')) {
-    //             possible_directions.push(directions_before[i])
-    //         }
-    //     }
-    //     let new_directions = []
-    //     for(let i=0; i<possible_directions.length; i++) {
-    //         if((!prev_directions.includes(possible_directions[i])) && (possible_directions[i] != -direction)) {
-    //             new_directions.push(possible_directions[i])
-    //         }
-    //     }
-    //     // if(new_directions.includes(-direction))
-    //     prev_directions = new_directions
-    //     if(new_directions.length>0) {
-    //         direction = new_directions[Math.floor(Math.random()*new_directions.length)]
-    //     }
-    //     //krok, sprawdzenie czy byly ball na nastepnym polu, i czy byl player
-    //     enemy_position += direction
-    //     board[enemy_position].classList.contains('ball') ? condition = true : condition = false
-    //     board[enemy_position].classList.remove('ball')
-    //     board[enemy_position].classList.add('enemy')
-    //     if(enemy_position==position) {
-    //         clearInterval(enemy_move)
-    //         document.removeEventListener('keydown', move)
-    //         lose.style.display = 'flex'
-    //         board[position].classList.remove('player')
-    //         lose.addEventListener('click', function() {
-    //             window.location.reload(true)
-    //         })
-    //         score.innerHTML = 'lipa :/'
-    //         comment.innerHTML = ''
-    //     }
-    // }
-    // const enemy_move = setInterval(run, 200)
-
-    
-
-    //chodzenie 
-    // let position = 220
+    //set direction on keydown 
     function move(k) {
+        board[playerPosition].classList.remove('open-right', 'open-left', 'open-down','open-up')
         switch(k.keyCode) {
             case 37:
-                if(!board[position-1].classList.contains('boarder')) {
-                    board[position].classList.remove('player')
-                    board[position].classList.add('path')
-                    position-=1
-                    ball()
-                    board[position].classList.add('player') 
-                }
+                playerDirection = -1
+                mouth = 'open-left'
+                moveDirection(playerDirection)
                 break
             case 38:
-                if(!board[position-size].classList.contains('boarder')) {
-                    board[position].classList.remove('player')
-                    board[position].classList.add('path')
-                    position-=size
-                    ball()
-                    board[position].classList.add('player') 
-                }
+                playerDirection = -size
+                mouth = 'open-up'
+                moveDirection(playerDirection)
                 break
             case 39:
-                if(!board[position+1].classList.contains('boarder')) {
-                    board[position].classList.remove('player')
-                    board[position].classList.add('path')
-                    position+=1
-                    ball()
-                    board[position].classList.add('player') 
-                }
+                playerDirection = 1
+                mouth = 'open-right'
+                moveDirection(playerDirection)
                 break
             case 40:
-                if(!board[position+size].classList.contains('boarder')) {
-                    board[position].classList.remove('player')
-                    board[position].classList.add('path')
-                    position+=size
-                    ball()
-                    board[position].classList.add('player') 
-                }
+                playerDirection = size
+                mouth = 'open-down'
+                moveDirection(playerDirection)
                 break
         }
         enemies.forEach(enemy => {
-            if(position==enemy.pozycja) {
-                // clearInterval(enemy_move)
-                document.removeEventListener('keydown', move)
-                lose.style.display = 'flex'
-                board[position].classList.remove('player')
-                board[position].classList.add('path')
-                lose.addEventListener('click', function() {
-                    window.location.reload(true)
-                })
-                score.innerHTML = 'lipa :/'
-                comment.innerHTML = ''
-                position = 0
+            if(playerPosition==enemy.enemyPosition) {
+                result.innerHTML = 'TOO BAD<br>RESTART'
+                endGame()
             }
         })
     }
     
     document.addEventListener('keydown', move)
 
-    // const tabela = [1,2,3,23,-23]
-
-    // tabela.forEach(t => {
-    //     if (t==3) {
-    //         console.log(t)
-    //         // break
-    // }
-    // })
-
-    // for(let i=0; i<tabela.length; i++) {
-    //     console.log(tabela[i])
-    //     if(tabela[i]==3){
-    //         break
-    //     }
-    // }
-
-    // console.log(Math.floor(42/21))
-    // 0 1
-    // 1 2
-    // 2 3
-    // 4 5
-    // jesli rzad enenmy < rzad player to idz w dół ewentualnie inne kierunku jak nie mozna 
-    // jesli > idz w gore ewentualnie inne kierunki jak nie mozna 
-
-    // let x = [1,2,3]
-
-    // console.log(x.includes(4))
 })
